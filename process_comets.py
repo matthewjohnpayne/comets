2cbb0a7dddfd725af40460364dcc3b57c51ca905
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-15 17:17:18 -0400

    first code commit

--- process_comets.py
+++ process_comets.py
@@ -63,7 +63,7 @@ def process_submission(obs_file):
         proc_dir = newsub.generate_subdirectory( 'comets' )
 
         # Set up a temp file name
-        tmp_obs_file = os.path.join(sub_dir , obs_file.split("/")[0] , str(n) )
+        tmp_obs_file = os.path.join(proc_dir , obs_file.split("/")[0] , str(n) )
         print("tmp_obs_file", tmp_obs_file)
         sys.exit()
         # Open temp file to allow us to write to it
